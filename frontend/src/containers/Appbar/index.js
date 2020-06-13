e34280349848c3dceb2f8e7f04fa717a9bfd82b9
Rosana Rezende <rezende_rosana@hotmail.com>
2020-06-12 23:26:56 -0300

    incluir logo

--- frontend/src/containers/Appbar/index.js
+++ frontend/src/containers/Appbar/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { push } from 'connected-react-router'
-import { AppBar, Toolbar, Typography} from "@material-ui/core"
+import { AppBar, Toolbar } from "@material-ui/core"
 import { routes } from "../../utils/constants"
+import * as S from "./styles"
 
 function Appbar() {
     const dispatch = useDispatch()
@@ -11,14 +12,11 @@ function Appbar() {
     return (
         <AppBar position="static">
             <Toolbar variant="dense">
-                <Typography 
-                    variant="h5" 
-                    color="inherit" 
+                <S.Logo
+                    src="https://user-images.githubusercontent.com/45580434/84555007-12291700-acf1-11ea-9b01-91d7f94f0755.png"
+                    alt="logo"
                     onClick={() => dispatch(goToHome)}
-                    style={ { cursor: "pointer" }}
-                >
-                    <img src="https://user-images.githubusercontent.com/45580434/84555007-12291700-acf1-11ea-9b01-91d7f94f0755.png" alt="logo"/>
-                </Typography>
+                />
             </Toolbar>
         </AppBar>
     )
---
+++ frontend/src/containers/Appbar/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+export const Logo = styled.img`
+    height: 30px;
+    cursor: pointer;
+`
